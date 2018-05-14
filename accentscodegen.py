3a6dc97985aa6b4541449eda19adfcd61d155bfd
Mark Varga <varga.mrk@gmail.com>
2018-05-14 09:17:32 -0700

    Add tooltip timeout variable to config

--- accentscodegen.py
+++ accentscodegen.py
@@ -6,22 +6,27 @@ config = configparser.ConfigParser()
 # parse config file
 config.read('config.ini')
 
-delayBeforeTooltip = config.getfloat('settings', 'delayBeforeTooltip')
+tooltips=config.getboolean('tooltips', 'tooltips')
+if tooltips:
+    delayBeforeTooltip = config.getfloat('tooltips', 'delayBeforeTooltip')
+    tooltipTimeout = int(config.getfloat('tooltips', 'tooltipTimeout') * 1000)
+
 chars = {"e":7, "y":1, "u":5, "i":6, "o":8, "a":8, "s":3, "l":1, "z":3, "c":3, "n":2}
 
 for char in chars:
     # this writes the tooltip displayer segment
-    print("~*{0}::".format(char)) 
-    print("    KeyWait, {0}, T{1}".format(char, delayBeforeTooltip))
-    print("    if ErrorLevel")
-    print("        if (GetKeyState(\"Shift\", \"P\")){")
-    print("            ToolTip, % {0}_upper_tooltip, %A_CaretX%, %A_CaretY%".format(char))
-    print("        } else {")
-    print("            ToolTip, % {0}_lower_tooltip, %A_CaretX%, %A_CaretY%".format(char))
-    print("        }")
-    print("        SetTimer, RemoveToolTip, 3000")
-    print("return")
-    print("")
+    if tooltips:
+        print("~*{0}::".format(char)) 
+        print("    KeyWait, {0}, T{1}".format(char, delayBeforeTooltip))
+        print("    if ErrorLevel")
+        print("        if (GetKeyState(\"Shift\", \"P\")){")
+        print("            ToolTip, % {0}_upper_tooltip, %A_CaretX%, %A_CaretY%".format(char))
+        print("        } else {")
+        print("            ToolTip, % {0}_lower_tooltip, %A_CaretX%, %A_CaretY%".format(char))
+        print("        }")
+        print("        SetTimer, RemoveToolTip, {0}".format(tooltipTimeout))
+        print("return")
+        print("")
     for n in range(1, chars[char] + 1):
         print("~{0} & {1}::".format(char, n))
         print("Send, {BackSpace}")
@@ -30,10 +35,8 @@ for char in chars:
         print("} else {")
         print("    Send, % {0}_lower[{1}]".format(char, n))
         print("}")
-        print("ToolTip")
+        if tooltips:
+            print("ToolTip")
         print("return")
         print("")
-input("") 
-# this "input("") function is to protect my mental health, by not requiring me to
-# find whatever's wrong with my current debugger (none of them seemed to give the
-# full proper console output)
+
