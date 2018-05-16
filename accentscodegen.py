e653710183f8e5eab0e7cb4d4faff9d70ed37801
Mark Varga <varga.mrk@gmail.com>
2018-05-16 22:30:16 +0200

    Change python code so that it writes directly to file

--- accentscodegen.py
+++ accentscodegen.py
@@ -1,5 +1,7 @@
 import configparser
 
+file = open('accents.ahk', mode="w", encoding="utf-8-sig")
+
 # TODO: add ability to select which programs should the script be enabled and disabled in
 
 # instantiate
@@ -22,9 +24,8 @@ chars = dict()
 for char in config["lowercase"]:
     chars[char] = len(config["lowercase"][char].split(","))
 
-print("#NoEnv")
-print("SendMode Input")
-print("")
+file.write("#NoEnv\n")
+file.write("SendMode Input\n\n")
 
 # Set Repeat and Slow Keys in Ease of Access Center to slow down keyboard repeat rates 
 # to allow for more time for the second keypress
@@ -35,7 +36,7 @@ print("")
 
 for char in config["lowercase"]:
     charList = config["lowercase"][char].replace(" ", "").split(",")
-    print(char + "_lower := " + str(charList).replace("\'","\""))
+    file.write(char + "_lower := " + str(charList).replace("\'","\"") + "\n")
     tooltip = charList[0]
     if len(charList) > 1:
         for index in range(1, len(charList)):
@@ -46,14 +47,14 @@ for char in config["lowercase"]:
         for index in range(2, len(charList) + 1):
             tooltip += "   "
             tooltip += str(index)
-    print(char + "_lower_tooltip := \"" + tooltip + "\"")
+    file.write(char + "_lower_tooltip := \"" + tooltip + "\"\n")
             
 
 # print uppercase
 
 for char in config["uppercase"]:
     charList = config["uppercase"][char].replace(" ", "").split(",")
-    print(char + "_upper := " + str(charList).replace("\'","\""))
+    file.write(char + "_upper := " + str(charList).replace("\'","\"") + "\n")
     tooltip = charList[0]
     if len(charList) > 1:
         for index in range(1, len(charList)):
@@ -64,39 +65,43 @@ for char in config["uppercase"]:
         for index in range(2, len(charList) + 1):
             tooltip += "   "
             tooltip += str(index)
-    print(char + "_upper_tooltip := \"" + tooltip + "\"")
+    file.write(char + "_upper_tooltip := \"" + tooltip + "\"\n")
+
+file.write("\n")
 
 # print code necessary for the key combinations
 
 for char in chars:
     # this writes the tooltip displayer segment
     if tooltips:
-        print("~*{0}::".format(char)) 
-        print("    KeyWait, {0}, T{1}".format(char, delayBeforeTooltip))
-        print("    if ErrorLevel")
-        print("        if (GetKeyState(\"Shift\", \"P\")){")
-        print("            ToolTip, % {0}_upper_tooltip, %A_CaretX%, %A_CaretY%".format(char))
-        print("        } else {")
-        print("            ToolTip, % {0}_lower_tooltip, %A_CaretX%, %A_CaretY%".format(char))
-        print("        }")
-        print("        SetTimer, RemoveToolTip, {0}".format(tooltipTimeout))
-        print("return")
-        print("")
-    for n in range(1, chars[char] + 1):
-        print("~{0} & {1}::".format(char, n))
-        print("Send, {BackSpace}")
-        print("if (GetKeyState(\"Shift\", \"P\")) {")
-        print("    Send, % {0}_upper[{1}]".format(char, n))
-        print("} else {")
-        print("    Send, % {0}_lower[{1}]".format(char, n))
-        print("}")
-        if tooltips:
-            print("ToolTip")
-        print("return")
-        print("")
+        file.write("""~*{0}::
+KeyWait, {0}, T{1}
+    if ErrorLevel
+        if (GetKeyState(\"Shift\", \"P\")){{
+            ToolTip, % {0}_upper_tooltip, %A_CaretX%, %A_CaretY%
+        }} else {{
+            ToolTip, % {0}_lower_tooltip, %A_CaretX%, %A_CaretY%
+            }}
+SetTimer, RemoveToolTip, {2}
+return
 
-print("""RemoveToolTip:
+""".format(char, delayBeforeTooltip, tooltipTimeout))
+
+    for n in range(1, chars[char] + 1):
+        file.write("""~{0} & {1}::
+Send, {{BackSpace}}
+if (GetKeyState(\"Shift\", \"P\")) {{
+Send, % {0}_upper[{1}]
+}} else {{
+Send, % {0}_lower[{1}]
+}}""".format(char,n))
+        if tooltips:
+            file.write("ToolTip\n")
+        file.write("return\n\n")
+
+file.write("""RemoveToolTip:
 SetTimer, RemoveToolTip, Off
 ToolTip
 return""")
 
+file.close()
